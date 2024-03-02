17954d1e306f3627a4c378c2d924535e572a382b
jebomin <jebomin0515@naver.com>
2024-03-02 11:15:48 +0900

    #7.6

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+  const { id } = useParams();
+
+  const getMovie = async () => {
+    try {
+      const response = await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      );
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error("Error fetching movie:", error);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, [id]); // id 값이 변경될 때마다 getMovie 함수 호출
+
   return <h1>Detail</h1>;
 }
 
